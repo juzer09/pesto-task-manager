2ca0abdc5faa53ec672935be97f7c3f6d482737c
juzer09 <juzer@quickshiftdigital.com>
2024-04-09 15:03:13 +0530

    v1.2.0

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -44,7 +44,7 @@ function TaskForm({ addTask }) {
                     </label>
                     <select
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="status"
+                        id="status" as
                         value={task.status}
                         onChange={(e) => setTask({ ...task, status: e.target.value })}
                     >
